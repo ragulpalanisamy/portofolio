ec819866b365939568d5e7b8970a9be0f3245a4f
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-05-18 19:42:55 +0530

    Created a portofolio

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-
-fetch("https://jsonplaceholder.typicode.com/photos")
-        .then((response) => response.json())
-        .then((msg) => computeList(msg));
-
-      function computeList(movies){
-        let carddomList = ""; // initialize the variable to an empty string
-        movies.forEach((movie) => {
-          carddomList += `
-            <div class="card m-2">
-              <div class="card-header">
-                ${movie.title}
-              </div>
-              <div class="card-body">
-                <img src="${movie.thumbnailUrl}" alt="poster"/>
-              </div>
-            </div>
-          `;
-        });
-        document.getElementById("list-section").innerHTML = carddomList;
-      }
+let typed = new Typed(".multiple-text", {
+    strings : ["Frontend developer","perfectionalist","Intern @codosphere"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
