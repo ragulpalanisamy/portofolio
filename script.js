d5ada367bc34f33955059f0a691408a0d6ac115f
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-05-27 10:56:23 +0530

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings : ["Frontend developer","perfectionalist","Intern @codosphere"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
