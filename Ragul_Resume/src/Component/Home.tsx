970e21a554062f1b0fcbd1715036673478b66de9
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-06-24 21:38:34 +0530

    added the home page content

--- Ragul_Resume/src/Component/Home.tsx
+++ Ragul_Resume/src/Component/Home.tsx
@@ -1,13 +1,53 @@
+import React, { useEffect } from "react";
+import {
+  AiFillCodepenSquare,
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+import Typed from "typed.js";
+
 import "../index.css";
 
 export default function Home() {
+  useEffect(() => {
+    const typed = new Typed(".multiple-text", {
+      strings: ["Frontend developer", "perfectionalist", "Intern @codosphere"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+    });
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+        const navbarTop = document.getElementById("navbar_top");
+        navbarTop?.classList.add("fixed-top");
+
+        const navbarElement = document.querySelector(".navbar");
+        const navbarHeight = (navbarElement as HTMLElement)?.offsetHeight ?? 0;
+
+        document.body.style.paddingTop = `${navbarHeight}px`;
+      } else {
+        const navbarTop = document.getElementById("navbar_top");
+        navbarTop?.classList.remove("fixed-top");
+
+        document.body.style.paddingTop = "0";
+      }
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
-    <section className="home" id="home">
+    <section className="home mt-10" id="home">
       <section className="home-container">
         <h3>Hello, It's Me</h3>
         <h1>Ragul P</h1>
         <h3>
-          And I'm a<span className="multiple-text"></span>
+          And I'm a <span className="multiple-text"></span>
         </h3>
         <p>
           Experienced front-end developer proficient in HTML, CSS, JavaScript,
@@ -22,16 +62,18 @@ export default function Home() {
             href="https://www.linkedin.com/in/ragul-p-139346194/"
             className="my-media-1"
           >
-            <i className="bx bxl-linkedin"></i>
+            <AiFillLinkedin />
           </a>
           <a href="https://github.com/ragulpalanisamy" className="my-media-2">
-            <i className="bx bxl-github color-[#10a8f0]"></i>
+            <AiFillGithub />
           </a>
           <a href="https://codepen.io/ragulpalanisamy" className="my-media-3">
-            <i className="bx bxl-codepen color-[#10a8f0]"></i>
+            <AiFillCodepenSquare />
+          </a>
+          <a href="https://twitter.com/Ragul_1010" className="my-media-4">
+            <AiFillTwitterSquare />
           </a>
         </div>
-
         <div>
           <a href="/Ragul_Resume.pdf" className="btn">
             Download CV
